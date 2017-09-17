9a8a30735c4f569bf4ae056b6ea76b785583ec09
David-Rodden <davidrodden273@gmail.com>
2017-09-16 22:34:06 -0700

    Incorporating velocity with which the player can speed up/slow down fluidly

--- public/client/Entity.js
+++ public/client/Entity.js
@@ -2,26 +2,43 @@ const Entity = class {
     constructor(src, x, y) {
         let image = new Image();
         image.src = '/img/' + src;
-        this.bitmap = new createjs.Bitmap(image);
-        this.bitmap.x = x;
-        this.bitmap.y = y;
-        this.bitmap.regX = image.width / 2;
-        this.bitmap.regY = image.height / 2;
+        let bitmap = new createjs.Bitmap(image);
+        bitmap.x = x;
+        bitmap.y = y;
+        bitmap.scaleX = bitmap.scaleY = 0.25;
+        bitmap.regX = image.width / 2;
+        bitmap.regY = image.height / 2;
+        this._bitmap = bitmap;
+        this._dx = this._dy = 0;
     };
 
     get image() {
-        return this.bitmap;
+        return this._bitmap;
     }
 
     get position() {
         return this.bitmap.x + ', ' + this.bitmap.y;
     };
 
+    handleDirection(event) {
+        let bitmapX = this._bitmap.x, bitmapY = this._bitmap.y;
+        this._bitmap.rotation = Math.atan2(event.offsetY - bitmapY, event.offsetX - bitmapX) * 180 / Math.PI + 90;
+    }
+
+    handleKeyPress(event) {
+        if (event.keyCode === 38 || (event.keyCode === 40 && this._dy > 0)) this._dy -= 1;
+        if (event.keyCode === 40 || (event.keyCode === 38 && this._dy < 0)) this._dy += 1;
+        if (event.keyCode === 37 || (event.keyCode === 39 && this._dx > 0)) this._dx -= 1;
+        if (event.keyCode === 39 || (event.keyCode === 37 && this._dx < 0)) this._dx += 1;
+    };
+
     handleMovement(event) {
-        this.bitmap.rotation++;
-        if (event.keyCode === 38) this.bitmap.y -= 2;
-        if (event.keyCode === 40) this.bitmap.y += 2;
-        if (event.keyCode === 37) this.bitmap.x -= 2;
-        if (event.keyCode === 39) this.bitmap.x += 2;
+        this._bitmap.x += this._dx;
+        this._bitmap.y += this._dy;
+        if (!(event.keyCode === 37 || event.keyCode === 39))
+            this._dx += this._dx < 0 ? 1 : this._dx > 0 ? -1 : 0;
+        if (!(event.keyCode === 38 || event.keyCode === 40)) {
+            this._dy += this._dy < 0 ? 1 : this._dy > 0 ? -1 : 0;
+        }
     };
 };
--- public/client/canvas.js
+++ public/client/canvas.js
@@ -1,17 +1,19 @@
 'use strict';
 const stage = new createjs.Stage('cnv');
-const man = new Entity('man.png', 100, 40);
+const man = new Entity('green_player.png', 100, 40);
+createjs.Ticker.on('tick', function (event) {
+    man.handleMovement(event);
+});
 stage.addChild(man.image);
 $(document).ready(function () {
     stage.update();
 });
 $(document).bind('keydown', function (event) {
     "use strict";
-    man.handleMovement(event);
+    man.handleKeyPress(event);
     stage.update();
 });
 $('#cnv').bind('mousemove', function (event) {
-    let angle = Math.atan2(event.offsetY - man.bitmap.y, event.offsetX - man.bitmap.x) * 180 / Math.PI;
-    man.bitmap.rotation = angle;
+    man.handleDirection(event);
     stage.update();
 });
